e85ac4f16591ee8c240c621388ad330a06acad7d
Sebastien Attal <s.attal@bilendi.com>
2017-02-05 15:30:44 +0100

    Remove unused copy in deployment scripts

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Deployment script for dev environment
 
-cp -f web/app_dev.php web/index.php
-
 composer install
 php bin/console cache:clear
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -3,7 +3,5 @@
 
 export SYMFONY_ENV=prod
 
-cp -f web/app.php web/index.php
-
 composer install --no-dev --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
